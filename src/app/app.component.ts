45dc615a0d92d9e8a548211eed957142750dabef
Antonio Anzures <joaz@gft.com>
2020-01-07 13:29:26 -0600

    Ejercicio de calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculadora-con-angular';
+  titulo = 'Aplicacion de calculadora';
+  operandoA: number;
+  operandoB: number;
+  resultado:number;
+
+  onSumar():void{
+    this.resultado = this.operandoA + this.operandoB;
+  }
+  
 }
